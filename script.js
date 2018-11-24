3b1d706c98d2a0a62817c03e173afb13ccbecf96
GuillaumeLaine <guillaume.william1@hotmail.com>
2018-11-24 14:15:09 +0100

    Laine

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log('Hello World!')
+const surname = document.querySelector("#input_nom").querySelector("input");
+const name = document.querySelector("#input_prenom").querySelector("input");
+
+const submit = document.querySelector("#envoyer");
+submit.prevent
+submit.addEventListener('click', submitForm);
+
+function submitForm(event) {
+    let store = [];
+    data = document.querySelectorAll('.input_container');
+    for (let i = 0; i < data.length; i++) {
+        store.push(data[i]);
+    }
+    console.log("We collected the following data:", store);
+}
