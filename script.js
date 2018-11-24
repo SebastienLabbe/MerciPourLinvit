ab640a7f3502e2be609e5de5da50d6dd79c1e208
SebastienLabbe <sebazore.bob@gmail.com>
2018-11-24 17:59:05 +0100

    t

--- script.js
+++ script.js
@@ -4,12 +4,24 @@
 // Returns as a string the HTML source code for an input bar that holds text for the form (ex : Name )
 function create_form_text_holder(name,place_holder)
 {
+    let shown_name;
+    if(name != 'Des questions ou commentaires?')
+    {
+        shown_name = name + "<m style='color: red; display: inline-block;'> * </m>";
+    }
+    else
+    {
+        shown_name = name;
+    };
     if (typeof place_holder === 'undefined') 
     {
         place_holder = name; 
     };
+
+    
+
     return `<div class="input_container">
-                <label for="${name.toLowerCase()}"><b>${name} : </b></label>
+                <label for="${name.toLowerCase()}"><b> ${shown_name} : </b></label>
                 <br>
                 <input id="input_${name.toLowerCase()}" type="text" name="${name.toLowerCase()}" placeholder="${place_holder}">
                 <span class="focus-input" data-placeholder="${name.toUpperCase()}"></span>
@@ -57,6 +69,8 @@ function create_form_description()
             <br> <br>
             Toutes les informations collectées sont strictement pour l'association, elles ne 
             seront pas partagées et ne serviront que pour les besoins du projet.
+            <br> <br>
+            <m style='color: red; display: inline-block;'> * Champ obligatoire </m>
             </p>`;
 };
 
@@ -64,6 +78,7 @@ function create_form_description()
 function create_form_drop_down(name,elements)
 {
     let drops = [];
+    name += "<m style='color: red; display: inline-block;'> * </m>";
     for(let i = 0;i < elements.length ; i++)
     {
         drops.push(`<option value="${elements[i]}">${elements[i]}</option>`)
@@ -85,8 +100,7 @@ function create_invite_form(form_name)
 
     let form_inner_html = [];
 
-    let mandatory = "<span style='color: red; display: inline-block;'>* </span>"
-    let names = ['Nom','Prénom  ','Genre','Ville',
+    let names = ['Nom','Prénom','Genre','Ville',
     'Code Postal','Adresse','Numéro de téléphone',
     'Adresse mail', "Nombres d'habitants dans le foyer"];
 
@@ -96,18 +110,18 @@ function create_invite_form(form_name)
 
     for(let i = 0;i < names.length ; i++)
     {
-        form_inner_html.push(create_form_text_holder(names[i] + mandatory));
+        form_inner_html.push(create_form_text_holder(names[i]));
     };
 
     let name = "Modalités d'hébergement";
     let elements = ["Une chambre à part","Canapé-lit",
                     "Logement entier","Autre"];
-    form_inner_html.push(create_form_drop_down(name + mandatory ,elements));
+    form_inner_html.push(create_form_drop_down(name,elements));
 
     name = "Durée d'hébergement";
     elements = ["Deux semaines","De 1 mois à 3 mois",
                    "De 4 mois à 6 mois","6 mois ou plus"];
-    form_inner_html.push(create_form_drop_down(name + mandatory ,elements));
+    form_inner_html.push(create_form_drop_down(name,elements));
 
     names = ["A partir de quand pouvez-vous recevoir quelqu'un chez vous?",
                     "Qu'attendez-vous de cette expérience d'accueil ?",
@@ -117,9 +131,7 @@ function create_invite_form(form_name)
 
     for(let i = 0;i < names.length ; i++)
     {
-        if (i == 2) form_inner_html.push(create_form_text_holder(names[i]));
-        else form_inner_html.push(create_form_text_holder(names[i]) + mandatory);
-        
+        form_inner_html.push(create_form_text_holder(names[i]));
     };
 
     form_inner_html.push(create_form_button());
